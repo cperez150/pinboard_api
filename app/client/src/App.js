d2e12992fac4f4ace1ee8a2241830f9aaff5be55
Carmen Perez <carmenperez@Carmens-MacBook-Air.local>
2020-03-25 21:28:27 -0400

    updated folder structure

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -15,7 +15,7 @@ const trashIcon = <FontAwesomeIcon icon={faTrashAlt} size="2x" />;
 //URL
 let baseURL = process.env.REACT_APP_BASEURL;
 if (process.env.NODE_ENV === "development") {
-  baseURL = "http://localhost:3001/api/v1/";
+  baseURL = "http://localhost:3001";
 } else {
   baseURL = "https://sleepy-harbor-02264.herokuapp.com";
 }
